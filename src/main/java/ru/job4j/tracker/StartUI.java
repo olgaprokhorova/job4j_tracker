7363c4d65a5cd65ae307527851061c18b85b6988
olgaprokhorova <olya.prokhorova@gmail.com>
2020-11-15 00:44:49 +0300

    6. Date. Отображение даты. [#376106]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime itemDateTime = item.getCreated();
+        System.out.println(itemDateTime.format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+    }
+}
