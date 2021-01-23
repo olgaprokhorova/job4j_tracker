7e1dfa97628b70837f6c053cd22ab41142f46542
olgaprokhorova <olya.prokhorova@gmail.com>
2021-01-23 21:04:44 +0300

    2. Модель данных. [#376117]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Ivanov Ivan");
+        student.setGroup("KIM-1-2");
+        student.setEnrollmentDate(new Date());
+        System.out.println("Student: " + student.getFullName());
+        System.out.println("Group: " + student.getGroup());
+        System.out.printf("%1$s %2$td.%2$tm.%2$tY", "Enrollment date:", student.getEnrollmentDate());
+    }
+}
