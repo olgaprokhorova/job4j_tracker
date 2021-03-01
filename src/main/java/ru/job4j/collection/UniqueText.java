6d196a62277727a48354d86d3390f3844f761ec4
olgaprokhorova <olya.prokhorova@gmail.com>
2021-03-01 10:49:30 +0300

    2. Идентичные тексты [#376038]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -1,25 +1,21 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
 import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl = false;
+        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
         for (String o : origin) {
             check.add(o);
         }
-        int count = 0;
         for (String t : text) {
-            if (check.contains(t)) {
-               count++;
+            if (!check.contains(t)) {
+                rsl = false;
+                break;
             }
-        }
-        if (count == text.length) {
-            rsl = true;
         }
         return rsl;
     }
